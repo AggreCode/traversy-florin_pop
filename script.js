05b178aacbf92466f2df75d215d9656785b82228
BISWAJIT MOHANTY <biswajittlh@gmail.com>
2021-01-23 23:45:06 +0530

    rotataing page

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const container = document.querySelector('.container');
+const open = document.getElementById('open');
+const close = document.getElementById('close')
 
-const search = document.querySelector('.search');
-const input = document.querySelector('.input');
-const btn = document.querySelector('.btn');
-btn.addEventListener('click',()=>{
-    search.classList.toggle('active');
-    input.focus();
-});
+open.addEventListener('click',()=>
+    container.classList.add('show-nav')
+);
+close.addEventListener('click',()=>
+    container.classList.remove('show-nav')
+);
